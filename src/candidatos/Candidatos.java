53bbd02a352d811b297cbdfb9adfca8c403ec074
Ronald Jefrey Moreno Mora <morenoronald7@gmail.com>
2018-03-03 08:05:39 -0500

    Actualizacion 1
    actualizacion archivos

---
+++ src/candidatos/Candidatos.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package candidatos;
+
+/**
+ *
+ * @author Ronald
+ */
+public class Candidatos {
+    
+}
