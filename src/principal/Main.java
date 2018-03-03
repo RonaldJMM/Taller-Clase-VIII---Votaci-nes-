159b911c3a0b0fbdc1dc4126a64f755ed7c2f1e3
Unknown <luismiguel92_@hotmail.com>
2018-03-03 18:18:40 -0500

    actualizacion 14.1

--- src/principal/Main.java
+++ src/principal/Main.java
@@ -49,9 +49,91 @@ public class Main {
                 break;
             
             case 2:
+                 
+                /*--------------------------------------------------------------------------*/
+       
+                                                 do{/*repeticion del menu */
+                                                 System.out.println(".............................................");    
+                                                 System.out.println("         VOTACION AL CONGRESO CANDIDATOS.       ");
+                                                 System.out.println("    ...Elija el candidato de su preferencia...");
+                                                 System.out.println(".............................................");
+                                                 System.out.println("1.Candidato : "+"Partido:");
+                                                 System.out.println("2.Candidato : "+"Partido:");
+                                                 System.out.println("3.Candidato : "+"Partido:");
+                                                 System.out.println("4.Candidato : "+"Partido:");
+                                                 System.out.println("5.Candidato : "+"Partido:");
+                                                 System.out.println("6.Candidato : "+"Partido:");
+                                                 System.out.println("7.Candidato : "+"Partido:");
+                                                 System.out.println("8.Candidato : "+"Partido:");
+                                                 System.out.println("9.Candidato : "+"Partido:");
+                                                 System.out.println("10.Voto en blanco");
+                                                 System.out.println("..............................................");
+                                                 System.out.println("Digite el numero de la opcion que desea: ");
+        
+                                                 opcionMenu = lecturaVar.nextByte();/**escaneo del dato del menu del usuario*/
+                                                 switch(opcionMenu){
+            
+                                                 case 1:
+               
+                                                 
+                                                 
+                                                     break;
+            
+                                                 case 2:
+                    
                 
+                                                     break;
+               
+                                                  case 3:
                 
                
+                                                     break;
+               
+                                                  case 4:
+              
+                                                     break;
+           
+               
+                                                   case 5:
+               
+                                                     break;
+                                                     
+                                                     
+                                                    case 6:
+                    
+                
+                                                     break;
+               
+                                                  case 7:
+                
+               
+                                                     break;
+               
+                                                  case 8:
+              
+                                                     break;
+           
+                                                     
+               
+                                                   case 9:
+               
+                                                     break; 
+                                                     
+                                                   case 10:
+                                                       
+                                                       break;
+                                                             default:/**
+                                                                       *opcion no valida del menu.
+                                                                        * 
+                                                                        * */
+                                                    System.out.println("Opcion no valida en el menu.");
+                                                    break;
+                                                       }
+        
+                                                }while(opcionMenu!=1 && opcionMenu!=2 && opcionMenu!=3 &&opcionMenu!=4 && opcionMenu!=5 && opcionMenu!=6&&opcionMenu!=7 && opcionMenu!=8 && opcionMenu!=9&&opcionMenu!=10);/*condicion salida del menu.*/
+        
+                
+                /*--------------------------------------------------------------------------*/
                break;
                
             case 3:
